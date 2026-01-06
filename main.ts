55fdd086624d9533c054ec8f36a7d0da8c4655c9
mohamedbelhadj0990-creator <mohamedbelhadj0990@gmail.com>
2026-01-06 21:55:34 +0100

    that's toooo nice

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 999; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showIcon(IconNames.Heart)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Bonne année ")
+    basic.pause(100)
+    basic.showNumber(2026)
+})
 basic.forever(function () {
 	
 })
